7036dd6a826e1354b4563b4c80b247fa6af58797
Rodney Gitzel <rodney.gitzel@gmail.com>
2019-03-19 14:27:41 -0700

    formatting

--- runway/config/runway_config_schematic.py
+++ runway/config/runway_config_schematic.py
@@ -11,11 +11,11 @@ from schematics.types import BooleanType, DictType, ListType, LongType, ModelTyp
 
 
 VALID_REGIONS = [
-    "ap-south-1",
-
     "ap-northeast-1",
     "ap-northeast-2",
     "ap-northeast-3",
+
+    "ap-south-1",
 
     "ap-southeast-1",
     "ap-southeast-2",
@@ -55,14 +55,6 @@ ModuleEnvironmentConfigType = DictType(StringType)
 
 ModuleOptionsType = UnionType([StringType, ListType(StringType)])
 
-class ModuleDefinition(Model):
-    class_path = StringType(required=False)
-    environments = DictType(ModuleEnvironmentConfigType, required=False)
-    name = StringType(required=False)
-    options = DictType(ModuleOptionsType, required=False)
-    path = StringType(required=True)
-
-
 # each item in a deployment's list of modules can be either a string or a dict,
 #  which is difficult (if indeed possible) to define in a model... so instead
 #  before validation we turn strings into dicts...
@@ -73,46 +65,44 @@ class StringOrModuleModelType(ModelType):
         return super(StringOrModuleModelType, self).convert(value, context)
 
 
+class ModuleDefinition(Model):
+    class_path      = StringType(                           required=False)
+    environments    = DictType(ModuleEnvironmentConfigType, required=False)
+    name            = StringType(                           required=False)
+    options         = DictType(ModuleOptionsType,           required=False)
+    path            = StringType(                           required=True)
+
+
 class DeploymentDefinition(Model):
-    account_alias = DictType(StringType, required=False)
-    account_id = DictType(LongType, required=False, serialized_name='account-id')
-    assume_role = DictType(StringType, required=False, serialized_name='assume-role')
-    env_vars = DictType(ModuleEnvironmentConfigType, required=False)
-    environments = DictType(ModuleEnvironmentConfigType, required=False)
-    modules = ListType(StringOrModuleModelType(ModuleDefinition), required=True, min_size=1)
-    module_options = DictType(ModuleOptionsType, required=False)
-    regions = ListType(RegionType(), required=True, min_size=1)
-    skip_npm_ci = BooleanType(required=False, default=False, serialized_name='skip-npm-ci')
+    account_alias   = DictType(StringType,                                  required=False)
+    account_id      = DictType(LongType,                                    required=False, serialized_name='account-id')
+    assume_role     = DictType(StringType,                                  required=False, serialized_name='assume-role')
+    env_vars        = DictType(ModuleEnvironmentConfigType,                 required=False)
+    environments    = DictType(ModuleEnvironmentConfigType,                 required=False)
+    modules         = ListType(StringOrModuleModelType(ModuleDefinition),   required=True,  min_size=1)
+    module_options  = DictType(ModuleOptionsType,                           required=False)
+    regions         = ListType(RegionType(),                                required=True,  min_size=1)
+    skip_npm_ci     = BooleanType(                                          default=False,  serialized_name='skip-npm-ci')
 
-    def validate_account_alias(self, data, value):
-        _check_dict_keys_are_valid_environment_names('account_alias', data, False)
-        return value
+    def validate_environment_names(self, data, value):
+        # these cannot be '*'
+        for name in ['account_alias', 'account_id', 'assume_role']:
+            _check_dict_keys_are_valid_environment_names(name, data, False)
 
-    def validate_account_id(self, data, value):
-        _check_dict_keys_are_valid_environment_names('account_id', data, False)
-        return value
+        # these can be '*'
+        for name in ['env_vars', 'environments']:
+            _check_dict_keys_are_valid_environment_names(name, data, True)
 
-    def validate_assume_role(self, data, value):
-        _check_dict_keys_are_valid_environment_names('assume_role', data, False)
-        return value
-
-    def validate_env_vars(self, data, value):
-        _check_dict_keys_are_valid_environment_names('env_vars', data, True)
-        return value
-
-    def validate_environments(self, data, value):
-        _check_dict_keys_are_valid_environment_names('environments', data, True)
         return value
 
 
 class RunwayConfig(Model):
-    """Top-level config."""
-    deployments = ListType(ModelType(DeploymentDefinition), required=True, min_size=1)
-    ignore_git_branch = BooleanType(default=False)
+    deployments         = ListType(ModelType(DeploymentDefinition), required=True, min_size=1)
+    ignore_git_branch   = BooleanType(                              default=False)
 
 
 
-ENVIRONMENT_NAME_REGEX = re.compile("^[a-z][a-zA-Z0-9-_]+")
+ENVIRONMENT_NAME_REGEX = re.compile("^[a-z][a-zA-Z0-9-_]+$")
 
 def _check_dict_keys_are_valid_environment_names(node_name, data, allow_star):
     node = data.get(node_name) 
